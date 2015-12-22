ee50940da654c4f6ee199db3a9d3ea4b3d1da740
Carl Henningsson <Henningsson@Carls-Air.huawei.com>
2015-12-22 20:09:56 +0100

    complete prodjekt

--- FirstOwnApp/ViewController.swift
+++ FirstOwnApp/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var BigRed: UIImageView!
+    @IBOutlet weak var BigBlue: UIImageView!
+    @IBOutlet weak var SmallRed: UIImageView!
+    @IBOutlet weak var SmallBlue: UIImageView!
+    @IBOutlet weak var text: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func RedBird(sender: AnyObject) {
+        BigRed.hidden = false
+        BigBlue.hidden = true
+        SmallBlue.hidden = true
+        SmallRed.hidden = true
+        text.hidden = true
+    }
+    @IBAction func BlueBird(sender: AnyObject) {
+        BigBlue.hidden = false
+        BigRed.hidden = true
+        SmallRed.hidden = true
+        SmallBlue.hidden = true
+        text.hidden = true
+    }
+    @IBAction func ResetButton(sender: AnyObject) {
+        BigBlue.hidden = true
+        BigRed.hidden = true
+        SmallRed.hidden = false
+        SmallBlue.hidden = false
+        text.hidden = false
     }
 
 
